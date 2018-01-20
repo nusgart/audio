efd7c28b94640f1656f02b172943c6ecfff6293d
Nicholas Nusgart <nicknusgart@gmail.com>
2018-01-19 19:53:50 -0600

    Fix test

--- test.c
+++ test.c
@@ -1,15 +1,17 @@
 #include "AudioInterface.h"
 #include "stdio.h"
-
+#include <unistd.h>
 int main(int argc, char** argv){
   puts("Loading");
   initAudio();
+  usleep(100000);
   puts("inited audio");
   loadSound("s", "sh.mpcm");
   puts("loaded sounds");
   unsigned long int j=0;
   while(1){
     playSound("s", 1, 1, 1, 0,0,0, 1,1);
+    usleep(100000);
 //    j = 0;
     for(int i = 0; i < 100000000; i++)j+=i;
     if(j > 2l<<62l)break;
